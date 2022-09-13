3909b8cb134dbbd6235e7fc5b46b79a6f71fec7e
Phan Vinh Khanh <Khanh161297@gmail.com>
2022-09-13 09:17:26 +0700

    LinkedList in Java Collection

---
+++ LinkedList/src/fpt_udemy/academy/Main.java
@@ -0,0 +1,8 @@
+package fpt_udemy.academy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ UnitTest/src/test/java/com/example/UnitTest/MathUtilTest.java
@@ -0,0 +1,2 @@
+package com.example.UnitTest;public class MathUtilTest {
+}
